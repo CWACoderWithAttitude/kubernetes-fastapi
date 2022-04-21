d505806c78fb7c338d5de84972042ae312da7693
Volker Benders <vbenders@gmail.com>
2022-04-21 16:59:50 +0200

    add /ping to locust

--- locustfile.py
+++ locustfile.py
@@ -5,3 +5,6 @@ class LoadTesting(HttpUser):
     @task
     def hello_world(self):
         self.client.post("/api/v1/hello", json={})
+    @task
+    def hello_ping(self):
+        self.client.get("/ping")
